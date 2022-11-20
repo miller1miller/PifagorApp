cb78ad7e3bcc5f6e6179ff5f342d9a3f30805950
Yuriy Lemaykin <y.lemaykin@gmail.com>
2022-11-20 19:28:35 +0300

    Initial commit

---
+++ app/src/main/java/com/gmiller/pifagorapp/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.gmiller.pifagorapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.gmiller.pifagorapp.databinding.ActivityMainBinding
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun onClickResult(view: View){
+        if(!isFieldEmpty()){
+            val result = getString(R.string.result_info) + getResult()
+            binding.tvResult.text = result
+        }
+
+    }
+
+    private fun isFieldEmpty(): Boolean{
+        binding.apply{
+            if(edA.text.isNullOrEmpty()) edA.error = "заполните поле"
+            if(edB.text.isNullOrEmpty()) edB.error = "заполните поле"
+            return edA.text.isNullOrEmpty() || edB.text.isNullOrEmpty()
+        }
+    }
+
+    private fun getResult(): String{
+        val a: Double
+        val b: Double
+        binding.apply {
+            a = edA.text.toString().toDouble()
+            b = edB.text.toString().toDouble()
+        }
+        return sqrt((a.pow(2) + b.pow(2))).toString()
+    }
+}
